061d208dea0801765a29a12d6a67cfeb9bdac7f5
Quân Hoàng <quanhoang@Mories-Macbook.local>
2024-06-25 22:17:59 +0700

    delete token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -31,12 +31,7 @@ export const signup = async (req, res) => {
 				expiresIn: "15d",
 			});
 
-			res.cookie("jwt", token, {
-				maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-				httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-				sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-				secure: false, // do not
-			});
+			res.cookie("jwt", token)
 			await newUser.save();
 
 			res.status(201).json({
